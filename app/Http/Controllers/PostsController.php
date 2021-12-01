09e9f181a82bef76530ded95946250b5de9a3265
octaviolafourcade <octavio.lafourcade@gmail.com>
2021-12-01 15:14:20 -0500

    [ ADD ] Implement Eloquent for model creation [ UPDATE ] Use of firstOrFail instead of 404 validation

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use App\Models\Post;
 
 class PostsController extends Controller
 {
     public function show($slug)
     {
         // I want to select from the posts table where the slug column is = to what we get to the URI and give me the first result
-        $post = DB::table('posts')->where('slug', $slug)->first();
-
+        // $post = DB::table('posts')->where('slug', $slug)->first();
+        // Using Eloquent
+        $post = Post::where('slug', $slug)->firstOrFail();
         // To inspect a variable and kill the execution
         // dd($post);
         // $posts = [
@@ -19,9 +21,9 @@ class PostsController extends Controller
         //     'my-second-post' => 'Now I am geting the hang of this blogging thing'
         // ];
     
-        if (! $post){
-            abort(404, 'Sorry, that post was not found');
-        };
+        // if (! $post){
+        //     abort(404, 'Sorry, that post was not found');
+        // };
         return view('post', [
             'post' => $post
         ]);
